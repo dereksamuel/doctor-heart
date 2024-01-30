941ca2610527d5ddaabd6c6148f83d116e7b52ae
Derek Paul <derek.paul@brilliant360.net>
2024-01-30 14:30:26 -0500

    ultimate changes to code review

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// window.onload = async () => {
+//   const carData = await initializeModel();
+// };
 
-setupCounter(document.querySelector('#counter'))
+async function initializeModel() {
+  const response = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
+  if (response.status !== 200) {
+    console.error("[api-error]: There is an api error");
+    return;
+  }
+  const data = await response.json();
+  const cleaned = data.map(car => ({
+    mpg: car.Miles_per_Gallon,
+    horsepower: car.Horsepower,
+  }))
+  .filter(car => (car.mpg != null && car.horsepower != null));
+  console.log(data);
+  return cleaned;
+}
+
+async function run() {
+  // Load and plot the original input data that we are going to train on.
+  const data = await initializeModel();
+  const values = data.map(d => ({
+    x: d.horsepower,
+    y: d.mpg,
+  }));
+
+  tfvis.render.scatterplot(
+    {name: 'Horsepower v MPG'},
+    {values},
+    {
+      xLabel: 'Horsepower',
+      yLabel: 'MPG',
+      height: 300
+    }
+  );
+
+  // More code will be added below
+}
+
+document.addEventListener('DOMContentLoaded', run);
